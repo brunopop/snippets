6bcd47801bd7f340f51e6c0d2302a5b3adc29779
Bruno Pop-Stefanov <brunopop@gatech.edu>
2013-10-23 22:12:06 -0400

    made binary search a template function

--- search_alg/search_alg.cpp
+++ search_alg/search_alg.cpp
@@ -2,20 +2,21 @@
 #include <iostream>
 
 using namespace bps;
+using namespace std;
 
 int main(int argc, char* argv[])
 {
 	int array[32] = {-25, -24, -23, -20, -15, -10, -7, -5, -3, -2, 0, 3, 4, 5, 8, 11, 15, 16, 19, 20, 25, 27, 28, 29, 30, 35, 36, 37, 38, 39, 40, 41};
-	std::cout << "Array is:\n\t";
-	for (auto i : array)
-	{
-		std::cout << i << ", ";
-	}
-	std::cout << "\n\nEnter a number:" << std::endl;
+	cout << "Array is:\n\t";
+	for (auto i : array) cout << i << ", ";
+	cout << "\n\nEnter a number:" << endl;
 	int n;
-	std::cin >> n;
-	int index = FindIndex(array, 0, n);
-	std::cout << "The index of " << n << " is: " << index << std::endl;
+	cin >> n;
+	int index = BinarySearch(n, array, 0, 31);
+	if (index == -1)
+		cout << n << " was not found in the array." << endl;
+	else
+		cout << "The index of " << n << " is: " << index << endl;
 
 	return 0;
 }
--- search_alg/search_alg.h
+++ search_alg/search_alg.h
@@ -2,22 +2,23 @@
 
 namespace bps
 {
-	int FindIndex(int* array, int imin, int imax)
+	template<class T>
+	int BinarySearch(T& value, T* array, int imin, int imax)
 	{
 		if (imin == imax)
 		{
-			if (array[imin] == imin)
+			if (array[imin] == value)
 				return imin;
 			else
 				return -1;
 		}
-		int icenter = (imin + imax) / 2;
-		if (array[icenter] == icenter)
+		int icenter = imin + (imax - imin)/2;
+		if (array[icenter] == value)
 			return icenter;
-		else if (array[icenter] < icenter)
-			FindIndex(array, icenter+1, imax);
+		else if (value < array[icenter])
+			return BinarySearch(value, array, imin, icenter);
 		else
-			FindIndex(array, imin, icenter);
+			return BinarySearch(value, array, icenter+1, imax);
 	};
 }
 
